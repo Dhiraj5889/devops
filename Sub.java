fe27db10d56e794beefb2cb63190bc08f4a4fd0f
Dhiraj <kakaded496@gmail.com>
2025-09-03 16:07:16 +0530

    hii

---
+++ Sub.java
@@ -0,0 +1,12 @@
+public class Sub {
+    public static void main(String[] args) {
+        int number1 = 20;
+        int number2 = 10;
+        int difference = number1 - number2;
+        
+        System.out.println("Simple Java Calculator");
+        System.out.println("Number 1:       " + number1);
+        System.out.println("Number 2:       " + number2);
+        System.out.println("Difference:     " + difference);
+    }
+}
