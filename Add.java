f6f07ec09d1b77237e880f569e84058b510df7cf
Dhiraj <kakaded496@gmail.com>
2025-09-03 15:37:08 +0530

    add file added

--- Add.java
+++ Add.java
@@ -1,12 +1,11 @@
-public class SimpleCalculator {
+public class Add {
     public static void main(String[] args) {
         int number1 = 10;
         int number2 = 20;
         int sum = number1 + number2;
         
         System.out.println("Simple Java Calculator");
-        System.out.println("Number 1:       " + number1);
-        System.out.println("Number 2: " + number2);
+        
         System.out.println("Sum: " + sum);
     }
 }
